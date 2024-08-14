204dffe0699207ecb002b79f18fe86a68ac3a9bb
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2024-08-14 17:52:14 +0900

    first commit

---
+++ src/main/java/com/iidx/scoreboard/IidxScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.iidx.scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IidxScoreBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IidxScoreBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iidx/scoreboard/IidxScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iidx.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IidxScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
