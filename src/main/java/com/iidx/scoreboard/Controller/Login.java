bc7dd90590c131a5d9b70ffd704b1f2500711957
gpsrlin115 <lefd123@gmail.com>
2024-08-21 13:49:18 +0900

    feat: login logic

---
+++ src/main/java/com/iidx/scoreboard/Controller/Login.java
@@ -0,0 +1,4 @@
+package com.iidx.scoreboard.Controller;
+
+public class Login {
+}
---
+++ src/main/java/com/iidx/scoreboard/Repository/RegisterRepository.java
@@ -0,0 +1,7 @@
+package com.iidx.scoreboard.Repository;
+
+import com.iidx.scoreboard.Model.Register;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegisterRepository extends JpaRepository<Register, Long>{
+}
